835dd911b1b1976706309ddf5d624a9df8a58001
JoWaRo <jonathanwalterromero@gmail.com>
2024-08-04 12:37:00 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,40 @@
+Nombre: Jonathan Walter Romero
+
+Repositorio de GitHub: https://github.com/scrolldev/bc_71370-integrador_etapa1-Jonathan-Romero
+
+Link Netlify: https://spontaneous-gumdrop-5ddf4c.netlify.app/
+---------------------------------------------
+
+/* Estructura 7 en 1.
+! base -> todos los estilos de inicio
+-> _reset.scss
+-> _typografhy.scss
+-> _variables.scss
+
+! components
+-> _buttons.scss
+-> _slider.scss
+-> _cards.scss
+
+! layout
+-> _header.scss
+-> _footer.scss
+-> _sidebar.scss
+
+! pages
+-> _home.scss
+-> _contacto.scss
+-> _nosotros.scss
+
+! themes
+-> _theme-dark.scss
+-> _theme-light.scss
+
+! utils
+-> _mixins.scss
+-> _functions.scss
+
+! vendors
+-> _bootstrap.scss
+-> _normalize.scss
+*/
---
+++ main.js
@@ -0,0 +1,47 @@
+import './sass/main.scss'
+import Handlebars from "handlebars";
+
+const start = async () => {
+  console.log("Hola mundo!")
+
+  try {
+
+    const respuesta = await fetch('templates/card.hbs')
+
+    if( !respuesta.ok ) {
+      throw new Error('No se pudo obtener la plantilla')
+    }
+
+    const plantilla = await respuesta.text()
+
+    console.log(plantilla)
+
+    
+    const template = Handlebars.compile(plantilla)
+
+    const respuestaBack = await fetch('http://localhost:8080/productos')
+
+    if (!respuestaBack.ok) {
+      throw new Error ('Algo paso con la respuesta!', respuestaBack.status)
+    }
+
+    const dataProductos = await respuestaBack.json()
+
+    const data = { productos: dataProductos }
+    console.log(data)
+    const html = template(data);
+
+    console.log(html)
+
+    const contenedorCards = document.querySelector('#contenedor-cards')
+
+    contenedorCards.innerHTML = html
+
+  } catch ( Error ) {
+
+  }
+}
+
+
+
+window.addEventListener('DOMContentLoaded', start)
